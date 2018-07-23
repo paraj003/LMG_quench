0a3e0eebeabbce84c10c4c8f8fba42c8e81690d0
Paraj Titum <paraj003@gmail.com>
2018-07-23 15:31:14 -0400

    updated job script for the cluster

--- pyjobscript.sh
+++ pyjobscript.sh
@@ -7,12 +7,22 @@
 #SBATCH --mail-type=END
 #SBATCH --mem-per-cpu=6999
 ##SBATCH --exclusive
-#SBATCH --array=1-5:
+#SBATCH --array=1-2
 . ~/.profile
 export MKL_NUM_THREADS=$SLURM_NTASKS
 export FFTW_NUM_THREADS=$SLURM_NTASKS
 
+module unload python
+module load python/3.5.1
+
 echo "
+
+import numpy as np
+from scipy import linalg as LA
+import scipy.sparse.linalg as spla
+import os
+import mod_LMG as LMG
+import h5py
 
 Larr=np.linspace(100,1000,10)
 L=Larr[int(os.environ[\"SLURM_ARRAY_TASK_ID\"])]  #Set system size.
@@ -22,7 +32,15 @@ dt=0.2 #time step
 Tf=20 # final time step
 Nsteps=int(Tf/dt) 
 tarr=np.arange(dt,Tf+dt,dt)
-">>LMG-params-$SLURM_JOB_ID.m
+">>LMG-params-$SLURM_JOB_ID.py
 
 
-cat LMG-params-$SLURM_JOB_ID.m cluster_LMGsim.m > cluster_LMGsim-$SLURM_JOB_ID.m
+cat LMG-params-$SLURM_JOB_ID.py cluster_LMGsim.py > temp-cluster_LMGsim-$SLURM_JOB_ID.py
+
+
+python3 temp-cluster_LMGsim-$SLURM_JOB_ID.py
+
+rm *$SLURM_JOB_ID.py
+
+hostname
+date
