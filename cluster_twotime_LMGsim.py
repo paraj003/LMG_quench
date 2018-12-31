9163f04351baffa54226d36e33e0426feafe2662
Paraj Titum <paraj003@gmail.com>
2018-12-31 16:11:39 -0500

    fixed filenames in cluster python script for two time correlation

--- cluster_twotime_LMGsim.py
+++ cluster_twotime_LMGsim.py
@@ -1,8 +1,11 @@
 directory1='data/Twotimecorrelation/'
 
-filename1=directory1+'Twotimecorrelator_Az_'+str(1.)+'_Ay_'+str(0.)+'_t1_['+str(t1arr[0])+'_'+str((t1arr[-1]-t1arr[0])/np.size(t1arr))+'_'+str(t1arr[-1])+']_t2_['+str(t2arr[0])+'_'+str((t2arr[-1]-t2arr[0])/np.size(t2arr))+'_'+str(t2arr[-1])+']_from_'+paramvals0.paramstr()+'_to_'+paramvalsf.paramstr()+'.hdf5'
-filename2=directory1+'Twotimecorrelator_Az_'+str(0.)+'_Ay_'+str(1.)+'_t1_['+str(t1arr[0])+'_'+str((t1arr[-1]-t1arr[0])/np.size(t1arr))+'_'+str(t1arr[-1])+']_t2_['+str(t2arr[0])+'_'+str((t2arr[-1]-t2arr[0])/np.size(t2arr))+'_'+str(t2arr[-1])+']_from_'+paramvals0.paramstr()+'_to_'+paramvalsf.paramstr()+'.hdf5'
-if not os.path.exists(filename1) or not os.path.exists(filename2):
+filename1=directory1+'Twotimecorrelator_Az_'+str(1.)+'_Ay_'+str(0.)+'_t1_'+arrtostr(t1arr)+'_t2_'+arrtostr(t2arr)+'_from_'+paramvals0.paramstr()+'_to_'+paramvalsf.paramstr()+'.hdf5'
+filename2=directory1+'Twotimecorrelator_Az_'+str(0.)+'_Ay_'+str(1.)+'_t1_'+arrtostr(t1arr)+'_t2_'+arrtostr(t2arr)+'_from_'+paramvals0.paramstr()+'_to_'+paramvalsf.paramstr()+'.hdf5'
+
+if os.path.exists(filename1) and os.path.exists(filename2):
+    print("Simulation unnecessary. File:\n"+filename1+"\n"+filename2)
+else:
     #Initial state Ground state of the paramagnetic phase
     Ham0=LMG.LMG_generateHam(paramvals0)
     #(Ham0.transpose() == Ham0).all() #check hermitian
@@ -22,6 +25,4 @@ if not os.path.exists(filename1) or not os.path.exists(filename2):
         correlationmaty=LMG.twotimecorrelation(paramvals0,t1arr,t2arr,InitState,energyf,vecf,0,1)
         LMG.save_data_twotimecorrelation(paramvals0,paramvalsf,correlationmaty,t1arr,t2arr,0,1)
         print("Sy correlation done")
-else:
-    print("Simulation unnecessary. File:\n"+filename1+"\n"+filename2)
 
